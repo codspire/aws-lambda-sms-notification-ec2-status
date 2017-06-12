18f6d7468760cf1d3593c89bb45c3d3bd4b2bcec
Rakesh Nagar <codspire@gmail.com>
2017-06-12 15:55:20 -0400

    Update eod-ec2-alerts.py

--- eod-ec2-alerts.py
+++ eod-ec2-alerts.py
@@ -6,47 +6,50 @@ import boto3
 
 print('Loading function')
 
+
 def lambda_handler(event, context):
     TOPIC_ARN = os.environ['TOPIC_ARN']
     ec2Client = boto3.client('ec2')
-    
+
     response = ec2Client.describe_instances(
-		Filters=[
-				{
-					'Name': 'instance-state-name',
-					'Values': [
-						'pending',
-						'running'
-					]
-				},
-			],
+        Filters=[
+            {
+                'Name': 'instance-state-name',
+                        'Values': [
+                            'pending',
+                            'running'
+                        ]
+            },
+        ],
     )
-    
+
     instances = dict()
-    
+    instanceCount = 0
     for r in response['Reservations']:
-    	for i in r['Instances']:
-    		instanceType = i['InstanceType']
-    		if instanceType in instances:
-    			instances[instanceType] += 1
-    		else:
-    			instances[instanceType] = 1
-    
+        for i in r['Instances']:
+            instanceType = i['InstanceType']
+            instanceCount += 1
+            if instanceType in instances:
+                instances[instanceType] += 1
+            else:
+                instances[instanceType] = 1
+
     if len(instances) > 0:
-        msg = "%s active instance(s)\n" %(len(instances))
-        for k in sorted(instances, reverse=True):	
-            msg = "%s %s - %s\n" %(msg,instances[k],k)
-        
+        msg = "%s active instance(s)\n" % (instanceCount)
+        for k in sorted(instances, reverse=True):
+            msg = "%s %s - %s\n" % (msg, instances[k], k)
+
         print(msg)
-        
+
         snsClient = boto3.client('sns')
         snsResponse = snsClient.publish(
-    		TopicArn=TOPIC_ARN,
-    		Message=msg
-    	)
+            TopicArn=TOPIC_ARN,
+            Message=msg
+        )
         print(snsResponse)
     else:
-    	print("No instance is running")    
+        print("No instance is running")
+
 
 # Enable execution of Lambda functionally from local machine
 # Note: export the TOPIC_ARN environment variable before running this function
